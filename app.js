54646788f14f3dcbe6b5626cafc6de20de451b42
Francys Garcia <fmgarciadeveloper@gmail.com>
2018-11-12 22:21:40 -0300

    add end-points initial

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* 
+  get home
+*/
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
